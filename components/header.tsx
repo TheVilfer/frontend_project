625c51594e12129d848be7772d556e25ec4b0186
AndrewLevada <levada.andrew@gmail.com>
2024-07-22 12:27:50 +0300

    changed header when user is authed

--- components/header.tsx
+++ components/header.tsx
@@ -5,13 +5,34 @@ import { MenuIcon, XIcon } from '@heroicons/react/outline';
 
 const Header: React.FC = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const { data: session } = useSession()
 
   return (
     <header className="bg-white shadow-md">
       <div className="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8 flex justify-between items-center">
         <h1 className="text-2xl font-bold">Quantum Hedgehogs</h1>
         <div className="flex items-center">
-          <nav className="hidden md:flex space-x-4">
+
+        {session ? (
+              <nav className="hidden md:flex space-x-4">
+              <Link href="/wardrope">
+                <span className="text-gray-600 hover:text-gray-800 mx-2">
+                  Wardrope
+                </span>
+              </Link>
+              <Link href="/add">
+                <span className="text-gray-600 hover:text-gray-800 mx-2">
+                  Add item
+                </span>
+              </Link>
+              <Link href="/generate">
+                <span className="text-gray-600 hover:text-gray-800 mx-2">
+                  Generate outfit
+                </span>
+              </Link>
+            </nav>
+            ) : (
+              <nav className="hidden md:flex space-x-4">
             <Link href="/#features">
               <span className="text-gray-600 hover:text-gray-800 mx-2">
                 Features
@@ -43,6 +64,8 @@ const Header: React.FC = () => {
               </span>
             </Link>
           </nav>
+            )}
+
           <div className="md:hidden">
             <button onClick={() => setIsMenuOpen(!isMenuOpen)}>
               {isMenuOpen ? (
@@ -57,56 +80,58 @@ const Header: React.FC = () => {
       </div>
       {isMenuOpen && (
         <div className="md:hidden bg-white shadow-md">
-          <nav className="flex flex-col items-center space-y-4 py-4">
+        {session ? (
+              <nav className="flex flex-col items-center space-y-4 py-4">
+              <Link href="/wardrope">
+                <span className="text-gray-600 hover:text-gray-800 mx-2">
+                  Wardrope
+                </span>
+              </Link>
+              <Link href="/add">
+                <span className="text-gray-600 hover:text-gray-800 mx-2">
+                  Add item
+                </span>
+              </Link>
+              <Link href="/generate">
+                <span className="text-gray-600 hover:text-gray-800 mx-2">
+                  Generate outfit
+                </span>
+              </Link>
+            </nav>
+            ) : (
+              <nav className="flex flex-col items-center space-y-4 py-4">
             <Link href="/#features">
-              <span
-                onClick={() => setIsMenuOpen(false)}
-                className="text-gray-600 hover:text-gray-800"
-              >
+              <span className="text-gray-600 hover:text-gray-800 mx-2">
                 Features
               </span>
             </Link>
             <Link href="/#how-it-works">
-              <span
-                onClick={() => setIsMenuOpen(false)}
-                className="text-gray-600 hover:text-gray-800"
-              >
+              <span className="text-gray-600 hover:text-gray-800 mx-2">
                 How It Works
               </span>
             </Link>
             <Link href="/#pricing">
-              <span
-                onClick={() => setIsMenuOpen(false)}
-                className="text-gray-600 hover:text-gray-800"
-              >
+              <span className="text-gray-600 hover:text-gray-800 mx-2">
                 Pricing
               </span>
             </Link>
             <Link href="/#testimonials">
-              <span
-                onClick={() => setIsMenuOpen(false)}
-                className="text-gray-600 hover:text-gray-800"
-              >
+              <span className="text-gray-600 hover:text-gray-800 mx-2">
                 Testimonials
               </span>
             </Link>
             <Link href="/#faq">
-              <span
-                onClick={() => setIsMenuOpen(false)}
-                className="text-gray-600 hover:text-gray-800"
-              >
+              <span className="text-gray-600 hover:text-gray-800 mx-2">
                 FAQ
               </span>
             </Link>
             <Link href="/about">
-              <span
-                onClick={() => setIsMenuOpen(false)}
-                className="text-gray-600 hover:text-gray-800"
-              >
+              <span className="text-gray-600 hover:text-gray-800 mx-2">
                 About us
               </span>
             </Link>
           </nav>
+            )}
         </div>
       )}
     </header>
@@ -119,9 +144,6 @@ function UserAuth() {
   if (status === 'authenticated') {
     return (
       <div className="ml-5 flex items-center">
-        <Link href="/app">
-          <span className="text-gray-600 hover:text-gray-800 mx-2">App</span>
-        </Link>
         <Link href="/api/auth/signout">
           <span className="text-gray-600 hover:text-gray-800 mx-2">
             Sign Out
